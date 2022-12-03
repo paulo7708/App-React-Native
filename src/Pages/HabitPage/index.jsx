22e34083d7ade71fcc7289a9ca320427bd40459f
Paulo7708 <dawnbreakerph@gmail.com>
2022-12-03 13:44:02 -0300

    Notificação toggle e texto

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -1,13 +1,16 @@
 import { useNavigation } from "@react-navigation/native";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, setHabitInput } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, Alert } from "react-native";
 import SelectFrequency from "../../Components/HabitPage/SelectFrequency";
 import SelectHabit from "../../Components/HabitPage/SelectHabit";
+import Notification from "../../Components/HabitPage/Notification";
+
 
 export default function HabitPage( {route} ){
   const navigation = useNavigation();
   const [habitInput, setHabitInput] = useState();
   const [frequencyInput, setFrequencyInput] = useState();
+  const [notificationToggle,setNotificationToggle] = useState();
   
   const { create, habit } = route.params;
 
@@ -36,6 +39,12 @@ export default function HabitPage( {route} ){
               habitFrequency={habit?.habitFrequency}
               frequencyInput={setFrequencyInput}
             />
+            {frequencyInput === "Mensal" ? null : (
+              <Notification
+                notificationToggle={notificationToggle}
+                setNotificationToggle={setNotificationToggle}
+              />
+            )}
           </View>
         </View>
       </ScrollView>
