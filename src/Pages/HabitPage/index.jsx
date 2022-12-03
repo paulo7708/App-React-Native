9acd7cad5138e7d5dfdce416e12ee7a6a4b73590
Paulo7708 <dawnbreakerph@gmail.com>
2022-12-03 10:06:35 -0300

    createHabit e estrutura de rotas

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -2,20 +2,29 @@ import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, Alert } from "react-native";
 
-export default function HabitPage(){
+export default function HabitPage( {route} ){
   const navigation = useNavigation();
+  const { create, habit } = route.params;
+
   return (
-    <View style={StyleSheet.container}>
+    <View style={styles.container}>
       <ScrollView>
         <View>
           <TouchableOpacity
             style={styles.backPageBtn}
             onPress={() => navigation.goBack()}
           >
-            <Image  source={require("../../assets/icons/arrowBack.png")}
+            <Image source={require("../../assets/icons/arrowBack.png")}
+            style={styles.arrowBack}
             />
           </TouchableOpacity>
-          <View style={styles.mainContent}></View>
+          <View style={styles.mainContent}>
+            <Text style={styles.title}>Configurações {"\n"} de hábito</Text>
+            <Text style={styles.inputText}>Área</Text>
+            <View style={styles.inputContainer}>
+              <Text style={styles.area}>{habit?.habitArea}</Text>
+            </View>
+          </View>
         </View>
       </ScrollView>
     </View>
@@ -27,7 +36,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "rgba(21, 21, 21, 0.98)",
   },
-  bakcPageBtn: {
+  backPageBtn: {
     width: 40,
     height: 40,
     margin: 25,
@@ -39,5 +48,29 @@ const styles = StyleSheet.create({
   mainContent: {
     width: 250,
     alignSelf: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+    fontSize: 30,
+  },
+  inputText: {
+    color: "white",
+    fontSize: 16,
+    marginTop: 35,
+    marginBottom: 10,
+    marginLeft: 5,
+  },
+  inputContainer: {
+    borderWidth: 1,
+    borderColor: "#FFFFFF",
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  area: {
+    color: "#BBBBBB",
+    fontSize: 15,
   },
 })
